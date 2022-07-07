4491dccdf3e2366f46f76d920d442892ff512161
Baburam07 <babu.dahal07@gmail.com>
2022-07-07 12:55:44 +0545

    'fourth'

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   darkMode: false,
   content: [],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       fontFamily: {
         poppins: "Poppins",
@@ -11,14 +18,6 @@ module.exports = {
       colors: {
         bcolor: "#FF6100",
         tcolor: "#AEA299",
-      },
-      screens: {
-        xs: "480px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",
-        "2xl": "1536px",
       },
     },
   },
