fae68275f8580b04c8850bb42f97545525e1c482
Baburam07 <babu.dahal07@gmail.com>
2022-07-19 18:47:58 +0545

    'changed'

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,17 @@ module.exports = {
         client: "#6B6B6B",
         fcolor: "#F5C9B04D",
         pricecolor: "#F5894E",
+        testcolor: "#273150",
+        bgtestcolor: "#FDFFF1",
+      },
+      lineHeight: {
+        loose: "30px",
+      },
+      fontWeight: {
+        fourFifty: "450",
+      },
+      boxShadow: {
+        priceShadow: " 0px 12px 30px rgba(8, 10, 10, 0.08)",
       },
     },
   },
